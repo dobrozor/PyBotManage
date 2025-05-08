3b89f8d7b782cf64486af431d6ce857be6989565
Илья Зоренко <105580858+dobrozor@users.noreply.github.com>
2025-05-08 12:00:42 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Установка и обновление пакетов
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install -y openssh-server ufw python3.10-venv nano
+
+# Настройка firewall
+sudo ufw enable
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 5000
+sudo ufw allow 443
+sudo ufw allow 8080
+
+# Создание директорий и виртуального окружения
+mkdir -p ~/bots/systemd
+cd ~/bots
+python3 -m venv venv
+source venv/bin/activate
+
+# Создание service файла
+cat <<EOF > ~/bots/systemd/site.service
+[Unit]
+Description=WebSite PythonManager
+After=syslog.target
+After=network.target
+ 
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/root/bots/site
+ExecStart=/root/bots/venv/bin/python3 /root/bots/site/bot.py
+RestartSec=5
+Restart=always
+ 
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Установка Python зависимостей
+pip install flask os-subprocess functools shutil
+
+# Настройка и запуск сервиса
+sudo systemctl enable /root/bots/systemd/site.service
+sudo systemctl start site
+
+echo "Твой сайт готов! Переходи по айпи адресу твоего сервера."
