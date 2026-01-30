288b68ca24049a0af8346a5ef2f8ac720b72a602
Илья Зоренко <105580858+dobrozor@users.noreply.github.com>
2026-01-30 11:27:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ source venv/bin/activate
 
 # Установка Flask и зависимостей
 echo -e "${ORANGE}[5/8] Установка зависимостей Python...${NC}"
-pip install flask paramiko flask_socketio
+pip install flask paramiko flask_socketio flask_login
 
 # Загрузка файлов проекта
 echo -e "${ORANGE}[6/8] Загрузка файлов проекта...${NC}"
