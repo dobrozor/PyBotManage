411879a272e1a363747571dd4416886047c0f41c
Илья Зоренко <105580858+dobrozor@users.noreply.github.com>
2026-03-30 15:17:54 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -11,8 +11,11 @@ import shutil
 import logging
 from pathlib import Path
 
-
-
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 class ProjectManager:
     """Класс для управления проектами"""
